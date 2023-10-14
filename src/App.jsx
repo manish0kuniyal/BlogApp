c7002d1da1deca90493474a44a6b0f72030bbf80
Manish K <manishkuniyal24@gmail.com>
2023-10-15 00:13:39 +0530

    GoogleDrive

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
 import React from 'react'
-import {BrowserRouter as Router ,Route,Routes} from "react-router-dom"
-import Discover from './Components_/Discover'
-import Dashboard from './Components_/Dashboard'
-import Home from './Components_/Home'
-import LoginPage from './Components_/LoginPage'
+
 const App = () => {
   return (
     <div>
-      <Router>
-      <Routes>
-        <Route path='/' element={<LoginPage/> }/>
-        <Route path='/Home' element={<Home/>}/>
-        <Route path='/discover' element={<Discover/>}/>
-      </Routes>
-      </Router>
+      App
     </div>
   )
 }
